3163f8fb5d0e4b9c615aacfcce3fd19d82d1b29e
Latok <jamedina@gmail.com>
2015-05-02 11:47:40 +0100

    no message

---
+++ NewOldsIntro/NewOldsIntro/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  NewOldsIntro
+//
+
+//  Copyright (c) 2015 NewOlds. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ gamedev/Pipes/Pipes/MenuScene.m
@@ -0,0 +1,13 @@
+//
+//  MenuScene.m
+//  Pipes
+//
+//  Created by Juan Antonio Medina Iglesias on 02/05/2015.
+//  Copyright (c) 2015 NewOlds. All rights reserved.
+//
+
+#import "MenuScene.h"
+
+@implementation MenuScene
+
+@end
